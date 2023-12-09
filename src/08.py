449f444279e78dcf3ee7f0232ef9be85dd3f1627
becauselol <becauselol@github.com>
2023-12-09 10:26:16 +0800

    did day 8 part 1

---
+++ src/08.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from utils.api import get_input
+import contextlib
+
+input_str = get_input(8)
+
+def solve(input_str):
+    # WRITE YOUR SOLUTION HERE
+    string_arr = input_str.split("\n")
+    string_arr = string_arr[:-1]
+    steps = string_arr[0]
+    string_arr = string_arr[2:]
+    
+    dd = defaultdict(dict)
+    for line in string_arr:
+        start, nodes = line.split("=")
+        start, nodes = start.strip(), nodes.strip()
+        l, r = nodes.split(",")
+        l, r = l.strip(), r.strip()
+        dd[start]["L"] = l[1:]
+        dd[start]["R"] = r[:-1]
+
+    n = len(steps)
+    res = 0
+    node = "AAA"
+    
+    while node != "ZZZ":
+        node = dd[node][steps[res % n]]
+        res += 1
+    print(res)
+
+    nodes = [k for k in dd.keys() if k[-1] == "A"]
+    res = 0
+    while any([n[-1] != "Z" for n in nodes]):
+        for i in range(len(nodes)):
+            nodes[i] = dd[nodes[i]][steps[res % n]]
+        res += 1
+    print(res)
+
+
+
+with open("outputs/day_08.txt", "w+") as f:
+    with contextlib.redirect_stdout(f):
+        solve(input_str)
